1a415b2942fab33be7d0e3aa36a3d5587e3e42d5
Asher <asher@developthesouth.com>
2026-03-26 09:29:40 -0400

    Fix hydration issue: move stylesheets from _app to _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 import { SessionProvider } from 'next-auth/react'
 import { ToastProvider } from '@/components/Toast'
 
@@ -8,14 +7,6 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   return (
     <SessionProvider session={session || null}>
       <ToastProvider>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Playfair+Display:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
         <Component {...pageProps} />
       </ToastProvider>
     </SessionProvider>
